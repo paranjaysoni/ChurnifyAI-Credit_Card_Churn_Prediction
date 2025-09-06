fa4d5e92cc1983a8423e7dbf973d8b7b69370870
Paranjay Soni <soniparanjay@gmail.com>
2025-09-06 07:33:13 +0000

    Updated Files

--- app.py
+++ app.py
@@ -99,3 +99,10 @@ if st.button("🚀 Predict Churn"):
             "<h2 style='text-align:center; color: green;'>✅ The customer is likely to STAY</h2>",
             unsafe_allow_html=True,
         )
+
+# Footer Credit
+st.markdown("---")
+st.markdown(
+    "<h5 style='text-align: center; color: grey;'>Built with  ❤️  by <a href='https://github.com/paranjaysoni' target='_blank' style='color: grey; text-decoration: none;'>Paranjay Soni</a></h5>",
+    unsafe_allow_html=True
+)
